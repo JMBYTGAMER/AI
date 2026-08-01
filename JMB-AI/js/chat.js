ecd1102dd039e283b5c39bdbc56d77a36fe55311
JMBYTGAMER <jobinmathewbiju6@gmail.com>
2026-08-01 13:51:45 +0530

    Create chat.js

---
+++ JMB-AI/js/chat.js
@@ -0,0 +1,19 @@
+const sendButton = document.getElementById("sendButton");
+const messageInput = document.getElementById("message");
+const chatMessages = document.getElementById("chatMessages");
+
+sendButton.onclick = () => {
+
+    const text = messageInput.value.trim();
+
+    if(text==="") return;
+
+    chatMessages.innerHTML += `
+        <div class="user-message">
+            ${text}
+        </div>
+    `;
+
+    messageInput.value = "";
+
+};
